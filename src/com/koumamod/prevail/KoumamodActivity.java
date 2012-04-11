d1aaaf18f1ebd92901e51820774a840f95ef8459
Koumajutsu <koumajutsu@gmail.com>
2012-04-11 16:24:56 -0700

    now it works

--- src/com/koumamod/prevail/KoumamodActivity.java
+++ src/com/koumamod/prevail/KoumamodActivity.java
@@ -3,12 +3,17 @@ package com.koumamod.prevail;
 
 import android.app.Activity;
 import android.os.Bundle;
+//import android.view.View;
 import android.view.View;
-//import android.view.View.OnClickListener;
+import android.view.View.OnClickListener;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
+//import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import java.lang.Runtime;
+import java.io.*;
+import com.stericson.RootTools.RootTools;
+import android.widget.Toast;
+import android.content.SharedPreferences;
 
 //import android.os.storage.*;
 
@@ -18,16 +23,24 @@ public class KoumamodActivity extends Activity {
 	  private CheckBox checkDataMode;
     /** Called when the activity is first created. */
 	  Process p;
+	  public static final String PREFS_NAME = "KoumaModSettings";
+	  public String KoumaModFileData;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
         setContentView(R.layout.main);
-    try {
-        p = Runtime.getRuntime().exec("su");
-    } catch (java.io.IOException e){}
-        checkDataMode();
+        try {
+        	p = Runtime.getRuntime().exec("su");
+        } catch (java.io.IOException e){}
+		radioModGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+		checkDataMode = (CheckBox) findViewById(R.id.checkBox1);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+        checkDataMode.setClickable(settings.getBoolean("InternEna", false));
+        checkDataMode.setChecked(settings.getBoolean("MountIntern", false));
+        radioModGroup.check(settings.getInt("ModMode",R.id.radio0));
+        myDataMode();
     }
-    public void checkDataMode(){
+    public void myDataMode(){
 		radioModGroup = (RadioGroup) findViewById(R.id.radioGroup1);
 		radioModGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 			//@Override
@@ -35,6 +48,8 @@ public class KoumamodActivity extends Activity {
 				int selectedId = radioModGroup.getCheckedRadioButtonId();
 //				radioModButton = (RadioButton) findViewById(selectedId);
 				checkDataMode = (CheckBox) findViewById(R.id.checkBox1);
+		    	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		    	SharedPreferences.Editor editor = settings.edit();
 				switch (selectedId) {
 				case R.id.radio1:
 					checkDataMode.setClickable(true);
@@ -43,8 +58,64 @@ public class KoumamodActivity extends Activity {
 				case R.id.radio2:
 					checkDataMode.setClickable(false);
 					checkDataMode.setChecked(false);
+			    	editor.putBoolean("MountIntern", checkDataMode.isChecked());
 					break;
 				}
+		    	editor.putBoolean("InternEna", checkDataMode.isClickable());
+		    	editor.putInt("ModMode", selectedId);
+		    	editor.commit();
+		        RootTools.remount("/system/", "rw");
+		        KoumaModFileData = (
+		        		getString(R.string.modtext_header) + "\n" + ((checkDataMode.isChecked())? "" : getString(R.string.modtext_internal)) + 
+		        		((selectedId==R.id.radio1) ? getString(R.string.modtext_data) : "") +
+		        		getString(R.string.modtext_coreblock1) +
+		        		((selectedId==R.id.radio2) ? getString(R.string.modtext_ssm) : "") +
+		        		getString(R.string.modtext_coreblock2)
+		        		);
+		        editor.putString("KoumaModText", KoumaModFileData);
+		        editor.commit();
+		        try {
+		        	File outfile = new File("/system/koumamod");
+		        	if (outfile.canWrite()) {
+		        		FileWriter writer = new FileWriter(outfile);
+		        		writer.write(KoumaModFileData);
+		        		writer.flush();
+		        		writer.close();
+		        	}
+		        } catch (java.io.IOException e){}
+		        RootTools.remount("/system/", "ro");
+		        Toast.makeText(KoumamodActivity.this, "Settings saved.\nReboot for changes to take effect.", Toast.LENGTH_LONG).show();
+			}
+		});
+		checkDataMode.setOnClickListener(new OnClickListener() {
+	 
+			public void onClick(View v) {
+				int selectedId = radioModGroup.getCheckedRadioButtonId();
+		    	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		    	SharedPreferences.Editor editor = settings.edit();
+		    	editor.putBoolean("MountIntern", checkDataMode.isChecked());
+		    	editor.commit();
+		        RootTools.remount("/system/", "rw");
+		        KoumaModFileData = (
+		        		getString(R.string.modtext_header) + ((checkDataMode.isChecked())? "" : getString(R.string.modtext_internal)) + 
+		        		((selectedId==R.id.radio1) ? getString(R.string.modtext_data) : "") +
+		        		getString(R.string.modtext_coreblock1) +
+		        		((selectedId==R.id.radio2) ? getString(R.string.modtext_ssm) : "") +
+		        		getString(R.string.modtext_coreblock2)
+		        		);
+		        editor.putString("KoumaModText", KoumaModFileData);
+		        editor.commit();
+		        try {
+		        	File outfile = new File("/system/koumamod");
+		        	if (outfile.canWrite()) {
+		        		FileWriter writer = new FileWriter(outfile);
+		        		writer.write(KoumaModFileData);
+		        		writer.flush();
+		        		writer.close();
+		        	}
+		        } catch (java.io.IOException e){}
+		        RootTools.remount("/system/", "ro");
+		        Toast.makeText(KoumamodActivity.this, "Settings saved.\nReboot for changes to take effect.", Toast.LENGTH_LONG).show();
 			}
 		});
     }
